884b9f46802b92d5a32ed940680696c041987fab
arastpour <89669082+arastpour@users.noreply.github.com>
2021-09-02 20:51:59 -0400

    Update function_BlockTridiagonalInverse2.m

--- function_BlockTridiagonalInverse2.m
+++ function_BlockTridiagonalInverse2.m
@@ -1,14 +1,14 @@
-%inplementation of (1)-(6) in Inversion of Block Tridiagonal Matrices Egil Kristoffer Gorm Hansen & Rasmus Koefoed Jakobsen Department of Computer Science – January 14th, 2010 Supervisor: Stig Skelboe 
+%inplementation of (1)-(6) in Inversion of Block Tridiagonal Matrices Egil Kristoffer Gorm Hansen & Rasmus Koefoed Jakobsen Department of Computer Science â€“ January 14th, 2010 Supervisor: Stig Skelboe 
 function g1 = function_BlockTridiagonalInverse2(ell, maxPhase, lambda1, lambda2, mu1, mu2, gamma1, gamma2, c, m)
-    %I will calculate g1, which includes blocks g1j of matrix G  =A^-1  
+    %We calculate g1, which includes blocks g1j of matrix G  =A^-1  
     Msize = maxPhase + 1;    
     N = m-1;% The original matrix that we want to calculate its inverse is of size N*Msize by N*Msize    
     a11 = function_Q(1, ell+1, maxPhase, lambda1, lambda2, mu1, mu2, gamma1, gamma2, c);
     d11L = a11;
     
     %calculate d11R
-    CR = zeros(Msize, N*Msize);% I will put c_iR,   i = N,...,2 in this matrix. NOTE: for convenience, I set the size of DR and CR to be (Msize, N*Msize) and not (Msize, (N-1)*Msize). Basically, I assume that c1R = d11R = 0. But I update d11R later.
-    %DR = zeros(Msize, N*Msize);% I will put d_iiR, i = N,...,2 in this matrix
+    CR = zeros(Msize, N*Msize);% We put c_iR,   i = N,...,2 in this matrix. NOTE: for convenience, we set the size of DR and CR to be (Msize, N*Msize) and not (Msize, (N-1)*Msize). Basically, we assume that c1R = d11R = 0. But we update d11R later.
+    %DR = zeros(Msize, N*Msize);% We put d_iiR, i = N,...,2 in this matrix
     aNN       = function_Q(1, ell+N  , maxPhase, lambda1, lambda2, mu1, mu2, gamma1, gamma2, c); 
     aNminus1N = function_Q(0, ell+N-1, maxPhase, lambda1, lambda2, mu1, mu2, gamma1, gamma2, c); 
     dNNR = aNN;
@@ -36,11 +36,3 @@ function g1 = function_BlockTridiagonalInverse2(ell, maxPhase, lambda1, lambda2,
         g1(:,(j-1)*Msize+1 : j*Msize) = g1_j_minus1 * c_j_R;
     end
 end
-% to check:
-% A = zeros( N*Msize , Msize);
-% A (1:2*Msize,:) = vertcat( a11 , a_2_1 );
-% g1 * A should be an Identity matrix of size Msize
-
-% B = zeros( N*Msize , Msize);
-% B ((N-2)*Msize+1:N*Msize,:) = vertcat( aNminus1N , aNN );
-%g1 * B should be zero matrix of size Msize
