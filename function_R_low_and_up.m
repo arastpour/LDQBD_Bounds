0fa1ea8477e9f7634c2fa75eeae08fae850a4ca3
arastpour <89669082+arastpour@users.noreply.github.com>
2021-09-02 20:58:24 -0400

    Update function_R_low_and_up.m

--- function_R_low_and_up.m
+++ function_R_low_and_up.m
@@ -2,9 +2,8 @@ function [R_lowerbar, R_upperbar, m] = function_R_low_and_up(epsilon_r,ell, maxP
     Msize = maxPhase+1;
     continue_r = true(1);
     m = m_initial;
-    %m = 200; %I could have started from 0, but I start from 2 because I refer to m-2 below
     while continue_r
-        W1 = zeros((m-1)*Msize, 2*Msize); %see Help_R calculations.jpg for more info about the structure of W1 and W2
+        W1 = zeros((m-1)*Msize, 2*Msize); 
         W1(1:Msize, 1:Msize)                           = function_Q(2, ell+1,   maxPhase, lambda1, lambda2, mu1, mu2, gamma1, gamma2, c); 
         W1((m-2)*Msize+1:(m-1)*Msize, Msize+1:2*Msize) = function_Q(0, ell+m-1, maxPhase, lambda1, lambda2, mu1, mu2, gamma1, gamma2, c); 
         V = -function_BlockTridiagonalInverse2(ell, maxPhase, lambda1, lambda2, mu1, mu2, gamma1, gamma2, c, m); % V calcualtes the rows 1:Msize of inv(W2)
