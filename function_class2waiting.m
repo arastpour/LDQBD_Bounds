90496f5f3f46a9ff09ce162b0181c5e0be17cb08
arastpour <89669082+arastpour@users.noreply.github.com>
2021-09-02 20:52:41 -0400

    Update function_class2waiting.m

--- function_class2waiting.m
+++ function_class2waiting.m
@@ -1,7 +1,7 @@
 function [f] = function_class2waiting(level,phase,c)
     %this function is prob of being in state (a,b)
     serverAvailable = max(c-phase,0);
-    if level >= serverAvailable % This function was wrong before because I had > instead of >=
+    if level >= serverAvailable 
         f = 1;
     else
         f=0;
