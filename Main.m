3215a70a93433d5ee49d3ac2964cedfdfcf2b1b2
arastpour <89669082+arastpour@users.noreply.github.com>
2021-09-02 20:50:02 -0400

    Update Main.m

--- Main.m
+++ Main.m
@@ -6,10 +6,10 @@ format longEng
 
 %parameter inputs
 mu1 = 1;
-mu2 = 1; %10
-gamma = 1;
-c = 100;
-rho = 0.95; 
+mu2 = 10;
+gamma = 10;
+c = 1;
+rho = 0.9; 
 propFromClass1 = 0.1; %it is $d$ in the paper
 lambda1 =    propFromClass1 * rho * c * mu1; %to calculate lambda 1 and 2 basedon other parameters
 lambda2 = (1-propFromClass1)* rho * c * mu2;
@@ -20,7 +20,7 @@ error = 0.01; %it is $\epsilon_s$ in the paper
 epsilon_aggrProb = 0.001; % It is $\epsilon_a$ in the paper
 paramV(1,:) = horzcat (1, rho, propFromClass1, lambda1, lambda2, mu1, mu2, gamma1, gamma2, c, epsilon_r, epsilon_h);
 
-cutOffPoint =60;%It is to preassign the size of x and R matrices. It does not actually need to be precise (as long as it is larger than 1). It is only to make sure that the size of upper_x_ellPrime_Matrix, lower_x_ellPrime_Matrix, upper_Pi_ellPrime_Matrix, and lower_Pi_ellPrime_Matrix do not change within the code. In case it is too small, the size of these matrices will start grwoing at each loop which will slow down the code. 
+cutOffPoint =60; %it is to preallocate the size of X and R matrices. It does not actually need to be precise (as long as it is larger than 1). It is only to make sure that the size of upper_x_ellPrime_Matrix, lower_x_ellPrime_Matrix, upper_Pi_ellPrime_Matrix, and lower_Pi_ellPrime_Matrix do not change within the code. In case it is too small, the size of these matrices will start grwoing at each loop which will slow down the code. 
 tau = inf; %it is $\ell^\tau$ in the paper
 offeredLoad = (lambda1/mu1 + lambda2/mu2)/c; 
 if offeredLoad >=1 && gamma1 == 0 && gamma2 == 0
@@ -33,13 +33,13 @@ upper_x_ellPrime_Matrix = inf * ones(cutOffPoint+1,maxPhase+1);
 lower_PI_ellPrime_Matrix = zeros(cutOffPoint+1,maxPhase+1); 
 upper_PI_ellPrime_Matrix = ones(cutOffPoint+1,maxPhase+1);
 indicatorV = zeros(cutOffPoint+1,1); %the ell-th element will indicate if the prob vector associated with level ell-1 satisfies the error tolerance 
-indicatorM = zeros(cutOffPoint+1,cutOffPoint+1); %I use this matirx just for debugging. At iteration k, I put indicatorV associated with this iteration in column k of this matrix
+indicatorM = zeros(cutOffPoint+1,cutOffPoint+1); %This matirx is for debugging purposes only. At iteration k, the vector indicatorV associated with this iteration is put in column k of this matrix
 startTime = tic;
 k = 0;    
 m_initial = 4;
 [lower_R0,upper_R0,m]=function_R_low_and_up(epsilon_r,0, maxPhase, lambda1, lambda2, mu1, mu2, gamma1, gamma2, c, m_initial);
 if m == -1, textToReport = 'Matrix W2 was close to singular'; function_writeToExcel(textToReport,lineNumber,runDate,paramV(problem,:),namesV); keyboard, end
-med_logic = min(min(upper_R0 - lower_R0)) < - eps || any(any(lower_R0 < -eps)); % I use eps instead of 0 to account for the floating-point relative error
+med_logic = min(min(upper_R0 - lower_R0)) < - eps || any(any(lower_R0 < -eps)); % We use eps instead of 0 to account for the floating-point relative error
 if med_logic
     disp (['ERROR: Matrix W2 is close to singular;  min(min(upper_R0 - lower_R0)) = ', num2str(min(min(upper_R0 - lower_R0))), '     min(min(lower_R0)) = ', min(min(lower_R0))] )%Note that depending on specs of your machine, you may get this message on some machines and not on others.
     keyboard
@@ -73,7 +73,7 @@ while  sum(lowerBoundSumV) < 1 - epsilon_aggrProb
         upper_xk = upper_xk_minus1 * upper_Rk_minus1;
         m_initial = m;
         [lower_Rk,upper_Rk,m]=function_R_low_and_up(epsilon_r,k, maxPhase, lambda1, lambda2, mu1, mu2, gamma1, gamma2, c, m_initial);
-        med_logic = min(min(upper_Rk - lower_Rk)) < - eps || any(any(lower_Rk < -eps)); % I use eps instead of 0
+        med_logic = min(min(upper_Rk - lower_Rk)) < - eps || any(any(lower_Rk < -eps)); 
         if med_logic
             disp (['min(min(upper_Rk - lower_Rk)) = ', num2str(min(min(upper_Rk - lower_Rk))), '     min(min(lower_Rk)) = ', min(min(lower_Rk))] )
             upper_PI_ellPrime = 0; lower_PI_ellPrime = 0;
@@ -103,7 +103,7 @@ while  sum(lowerBoundSumV) < 1 - epsilon_aggrProb
     end
 
     upper_c = ((upper_c1 + upper_c2));
-    lower_x_ellPrime_Matrix(k+1,:) = lower_xk; % I record the bounds for all x_ellPrime (unnormalized probs), but I will use only those that satisfy the torlerance error for calculating performance measures
+    lower_x_ellPrime_Matrix(k+1,:) = lower_xk; % We record the bounds for all x_ellPrime (unnormalized probs), but we will use only those that satisfy the torlerance error for performance measure calculation
     upper_x_ellPrime_Matrix(k+1,:) = upper_xk;
     lower_PI_ellPrime_Matrix = lower_x_ellPrime_Matrix ./upper_c;
     upper_PI_ellPrime_Matrix = upper_x_ellPrime_Matrix ./lower_c;
@@ -117,11 +117,11 @@ while  sum(lowerBoundSumV) < 1 - epsilon_aggrProb
     diffMat = abs(upper_PI_ellPrime_Matrix - lower_PI_ellPrime_Matrix) < error; % it is the matrix of differences between lower and upper bounds for all levels.
     indicatorV = all(diffMat,2); %if all elements of a row of diffMat is 1 (meaning that pi(level, phase), for all phase values, is within the tolerence error) then the indicator becomes 1. Otherwise it is 0.
     firstZeroInd = find(~indicatorV,1); %to find the index of the first element in indicatorV that is zero. In other words, for all levels below this index, the tolerance level is satisfied. NOTE: firstZeroInd - 1 is the index of the last non-zero element in indicatorV. And element i in indicatorV is associated with level i-1. For example, the first element is for level 0. Therefore, firstZeroInd - 2 is the highest level that we have its probs within the tolerance error
-    if (~isempty(firstZeroInd)  && firstZeroInd > 1 )   %sometimes, it is possible that, for example, pi for levels 3-10 is within the tolerence error, but pi for 0-2 are not. firstZeroInd gives me the first level for which the tolerence level is NOT satisfied. I calculate the sum of probs for all levels below that level and check if the sum is larger than 1 - epsilon_aggrProb. If it is, then I go out of the while loop. 
+    if (~isempty(firstZeroInd)  && firstZeroInd > 1 )   %sometimes, it is possible that, for example, pi for levels 3-10 is within the tolerence error, but pi for 0-2 is not. firstZeroInd gives the first level for which the tolerence level is NOT satisfied. We calculate the sum of probs for all levels below that level and check if the sum is larger than 1 - epsilon_aggrProb. If it is, then we go out of the while loop. 
         %all(indicatorV) means that all levels satisfy the prob.
         %tolerence level and there are NO zeros in the index vector.
         %(~isempty(firstZeroInd)  && firstZeroInd > 1 ) means that
-        %there is at least one level that does not satisfy the tol. and
+        %there is at least one level that does not satisfy the error tolerance and
         %that level is not level 0.
         lowerBoundSumV = sum(lower_PI_ellPrime_Matrix(1:firstZeroInd-1,:),2);
     elseif all(indicatorV)
@@ -141,7 +141,7 @@ while  sum(lowerBoundSumV) < 1 - epsilon_aggrProb
     tookTooLong = false(1);
     hours = 5;
     if toc(startTime)>3600*hours
-        lowerBoundSumV = 1; %it couses that the while loop terminates
+        lowerBoundSumV = 1; %it terminates the while loop
         tookTooLong = true(1);
     end
 end
